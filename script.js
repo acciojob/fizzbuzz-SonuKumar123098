d1f5157fde79bfedcbee986b6c4c559955aab4ca
SonuKumar123098 <112372023+SonuKumar123098@users.noreply.github.com>
2024-01-31 21:41:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 function print() {
-	for(let i=0;i<=20;i++)(
+	for(let i=1;i<=20;i++)(
 		if(i%3===0 && i%5===0){
 		    alert('FizzBuzz');
 		}else if(i%3===0)alert('Fizz');
